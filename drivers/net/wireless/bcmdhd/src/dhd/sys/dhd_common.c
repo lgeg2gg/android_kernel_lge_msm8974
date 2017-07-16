5283fdb2035c56c7521ed9b66681c44fd8d128ba
Nikola Majkić <majkforgit@gmail.com>
2017-07-17 01:05:47 +0200

    BCM43XX DHD 1.201.54 for L OS Release Note.
     DHD 1.201.54.3 (2014.12.26)
      - Patched kernel crash of SoftAP (CSP#870210)
      - Recovered link after dongle trap (CSP#865846)
      - Patched system watchdog bark problem of SoftAP (CSP#859423)
      - Patched to disable TDLS during P2P operation (CSP#860072)
      - Disable phy watchdog during Miracast for 4339 only (CSP#864058)
      - Allocated hanger memory to static buffer (CSP#867007)
      - Applied to accept P2P probe request during P2P negotiation if there is no peer device's P2P probe request (CSP#868459)
      - Patched wakelock problem on watchdog
    
    DHD 1.201.54.4 (2015.02.03)
      - Added private commands for WFDS; ADD_WFDS_HASH, DEL_WFDS_HASH (CSP#865694)
         (This needs FW's supporting)
      - Patched missing P2P IE for initial connection with LG DTV. (CSP#876196)
      - Removed pointer of packet filter's union member for 64bit host CPU (CSP#877380)
      - Added LPS feature (CSP#836412)
      - Fixed packet count routine for TCP ack suppress not to numerate suppressed acks as dropped packets (CSP#881850)
    
    DHD 1.201.54.5 (2015.03.20)
      - Added support for BCM43455
    
    DHD 1.201.54.6 (2015.03.23)
      - Fixed invalid FW ram base address at BCM43455

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_common.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_common.c
@@ -1,7 +1,7 @@
 /*
  * Broadcom Dongle Host Driver (DHD), common DHD core.
  *
- * Copyright (C) 1999-2014, Broadcom Corporation
+ * Copyright (C) 1999-2015, Broadcom Corporation
  * 
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
@@ -21,7 +21,7 @@
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
  *
- * $Id: dhd_common.c 515169 2014-11-13 13:19:49Z $
+ * $Id: dhd_common.c 520555 2014-12-12 07:18:27Z $
  */
 #include <typedefs.h>
 #include <osl.h>
@@ -261,6 +261,21 @@ const bcm_iovar_t dhd_iovars[] = {
 };
 
 #define DHD_IOVAR_BUF_SIZE	128
+
+void dhd_save_fwdump(dhd_pub_t *dhd_pub, void * buffer, uint32 length)
+{
+
+	if (dhd_pub->soc_ram == NULL) {
+		dhd_pub->soc_ram = (uint8*) MALLOCZ(dhd_pub->osh, length);
+		if (dhd_pub->soc_ram == NULL) {
+			DHD_ERROR(("%s: Failed to allocate memory for fw crash snap shot.\n",
+				__FUNCTION__));
+			return;
+		}
+	}
+	dhd_pub->soc_ram_length = length;
+	memcpy(dhd_pub->soc_ram, buffer, length);
+}
 
 /* to NDIS developer, the structure dhd_common is redundant,
  * please do NOT merge it back from other branches !!!
@@ -1648,7 +1663,7 @@ wl_show_host_event(dhd_pub_t *dhd_pub, wl_event_msg_t *event, void *event_data,
 #endif /* SHOW_EVENTS */
 
 int
-wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata, size_t pktlen,
+wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata, uint16 pktlen,
 	wl_event_msg_t *event, void **data_ptr, void *raw_event)
 {
 	/* check whether packet is a BRCM event pkt */
@@ -1659,19 +1674,17 @@ wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata, size_t pktlen,
 	int evlen;
 	int hostidx;
 
-	if (bcmp(BRCM_OUI, &pvt_data->bcm_hdr.oui[0], DOT11_OUI_LEN)) {
-		DHD_ERROR(("%s: mismatched OUI, bailing\n", __FUNCTION__));
-		return (BCME_ERROR);
-	}
-
-	/* BRCM event pkt may be unaligned - use xxx_ua to load user_subtype. */
-	if (ntoh16_ua((void *)&pvt_data->bcm_hdr.usr_subtype) != BCMILCP_BCM_SUBTYPE_EVENT) {
-		DHD_ERROR(("%s: mismatched subtype, bailing\n", __FUNCTION__));
-		return (BCME_ERROR);
-	}
-
 	if (pktlen < sizeof(bcm_event_t))
 		return (BCME_ERROR);
+
+	/* BRCM event pkt may be unaligned - use xxx_ua to load user_subtype. */
+	if (ntoh16_ua((void *)&pvt_data->bcm_hdr.subtype) != BCMILCP_SUBTYPE_VENDOR_LONG ||
+		(bcmp(BRCM_OUI, &pvt_data->bcm_hdr.oui[0], DOT11_OUI_LEN)) ||
+		ntoh16_ua((void *)&pvt_data->bcm_hdr.usr_subtype) != BCMILCP_BCM_SUBTYPE_EVENT)
+	{
+		DHD_ERROR(("%s: mismatched bcm_event_t info, bailing out\n", __FUNCTION__));
+		return (BCME_ERROR);
+	}
 
 	*data_ptr = &pvt_data[1];
 	event_data = *data_ptr;
@@ -1683,15 +1696,13 @@ wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata, size_t pktlen,
 	type = ntoh32_ua((void *)&event->event_type);
 	flags = ntoh16_ua((void *)&event->flags);
 	status = ntoh32_ua((void *)&event->status);
-
 	datalen = ntoh32_ua((void *)&event->datalen);
 	if (datalen > pktlen)
 		return (BCME_ERROR);
 
 	evlen = datalen + sizeof(bcm_event_t);
-	if (evlen > pktlen) {
+	if (evlen > pktlen)
 		return (BCME_ERROR);
-	}
 
 	/* find equivalent host index for event ifidx */
 	hostidx = dhd_ifidx2hostidx(dhd_pub->info, event->ifidx);
@@ -1699,6 +1710,7 @@ wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata, size_t pktlen,
 	switch (type) {
 #ifdef PROP_TXSTATUS
 	case WLC_E_FIFO_CREDIT_MAP:
+//LGE_Patch
 		if (dhd_wlfc_enable(dhd_pub) == BCME_OK) {
 			dhd_wlfc_FIFOcreditmap_event(dhd_pub, event_data);
 			WLFC_DBGMESG(("WLC_E_FIFO_CREDIT_MAP:(AC0,AC1,AC2,AC3),(BC_MC),(OTHER): "
